57bfb984c27b6af0f10a715c72711813dba43641
Venkata Pavan Kalyan Emmela <75110595+PavanKalyan91@users.noreply.github.com>
2021-01-21 21:01:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Basic-banking-system
+Sparks Foundation Internship Project : Basic Banking System 
+Stack used : 
+Front-end : HTML, CSS, Bootstrap and Javascript 
+Back-end : PHP 
+Database : MySQL
+Wamp Server(Local Host)
---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+   
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+   
+    <title>Basic Banking System Using PHP</title>
+  
+  </head>
+
+  <body>
+  <nav class="navbar navbar-expand-md  fixed-top bg-light tp">
+   
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </div>
+       </nav>
+  
+      <div class="container-fluid">
+            <div class="row  py-1">
+              <div class="col-sm-12 col-md">
+                <div class=" text-center my-5 ">
+                  
+                  <h1>WELCOME TO PRESIDENTIAL BANK</h1>
+                </div>
+              </div>
+            </div>
+            <div class="row activity text-center">
+                  <div class="col-md act usr">
+                    
+                    <br>
+                    <a href="transfermoney.php"><button class="btn btn-primary">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act usr">
+                   
+                    <br>
+                    <a href="transactionhistory.php"><button class="btn btn-primary">Transaction History</button></a>
+                  </div>
+            </div>
+      </div>
+     
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
